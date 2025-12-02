ffa2ab6fa4a6433af7affc9ba9bebf468892b905
joao lucas <111147564+joao322@users.noreply.github.com>
2025-12-02 17:11:00 -0300

    mundanca na variavel var

--- desafio.kt
+++ desafio.kt
@@ -1,12 +1,10 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60,)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
